cd21bd150bafab32b76515f94a35b005fd7f99eb
JvPenna76 <joao.penna@cxpbrasil.com.br>
2023-10-26 13:31:52 -0300

    ADD: cadastro não precisando mais de autenticação

--- matchmaker-api/src/main/java/com/example/matchmaker/matchmakerapi/api/configuration/security/SecurityConfiguracao.java
+++ matchmaker-api/src/main/java/com/example/matchmaker/matchmakerapi/api/configuration/security/SecurityConfiguracao.java
@@ -54,7 +54,8 @@ public class SecurityConfiguracao {
             new AntPathRequestMatcher("/actuator/*"),
             new AntPathRequestMatcher("/usuarios/login/**"),
             new AntPathRequestMatcher("/h2-console/**"),
-            new AntPathRequestMatcher("/error/**")
+            new AntPathRequestMatcher("/error/**"),
+            new AntPathRequestMatcher("/usuarios", HttpMethod.POST.toString())
     };
 
     @Bean

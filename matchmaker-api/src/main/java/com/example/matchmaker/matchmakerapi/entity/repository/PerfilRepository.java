256affeb085be580093bbe78f33e2b6c3af23896
estela-polverini <estela.polverini@sptech.school>
2023-11-13 21:28:31 -0300

    inclisão da chamada COMENTADA da proc de curtida no PerfilRepository

--- matchmaker-api/src/main/java/com/example/matchmaker/matchmakerapi/entity/repository/PerfilRepository.java
+++ matchmaker-api/src/main/java/com/example/matchmaker/matchmakerapi/entity/repository/PerfilRepository.java
@@ -2,8 +2,14 @@ package com.example.matchmaker.matchmakerapi.entity.repository;
 
 import com.example.matchmaker.matchmakerapi.entity.Perfil;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
 
 @Repository
 public interface PerfilRepository extends JpaRepository<Perfil, String> {
+    // @Procedure("SP_curtir_usuario")
+    // void curtirOutroUsuario(Integer idUsuarioLogado, Integer idUsuarioCurtido, LocalDate dataHora);
+
 }

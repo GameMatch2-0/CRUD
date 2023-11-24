ccddecaa643c42446786c5df47d9bf1ddaabc2f7
JvPenna76 <joao.penna@cxpbrasil.com.br>
2023-11-24 09:07:37 -0300

    ADD: Entidades Interesse e Interesse perfil

--- matchmaker-api/src/main/java/com/example/matchmaker/matchmakerapi/entity/InteressePerfil.java
+++ matchmaker-api/src/main/java/com/example/matchmaker/matchmakerapi/entity/InteressePerfil.java
@@ -1,4 +1,27 @@
 package com.example.matchmaker.matchmakerapi.entity;
 
+import com.example.matchmaker.matchmakerapi.entity.embeddable.InteressePerfilId;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class InteressePerfil {
+    @EmbeddedId
+    private InteressePerfilId interesseJogoPerfilId;
+    @ManyToOne
+    @JoinColumn(name = "idPerfil", insertable = false, updatable = false)
+    private Perfil perfil;
+    @ManyToOne
+    @JoinColumn(name = "idPerfil", insertable = false, updatable = false)
+    private Interesse InteresseId;
 }
